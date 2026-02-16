a66d4a53b3ac442c6bc92a4c41d6ca011be52af0
scott <scotthellman@fastmail.com>
2026-02-16 15:20:13 -0600

    for posterity, the latest version of the matmul experiment

--- experiments/matmul_kernel.py
+++ experiments/matmul_kernel.py
@@ -57,7 +57,6 @@ void coalescing_matmul(int m, int n, int p, const float* A, const float* B, floa
 """
 
 coalescing_kernel = cp.RawKernel(coalescing_matmul_code, "coalescing_matmul")
-# FIXME: think harder about x and y - why is my first version faster?
 
 shared_mem_matmul_code = r"""
 extern "C" __global__
@@ -113,6 +112,56 @@ void shared_matmul(int m, int n, int p, const float* A, const float* B, float* C
 """.replace("BLOCKSIZE", str(BLOCKSIZE))
 
 shared_kernel = cp.RawKernel(shared_mem_matmul_code, "shared_matmul")
+
+pointer_matmul_code = r"""
+extern "C" __global__
+void pointer_matmul(int m, int k, int n, const float* A, const float* B, float* C) {
+    const int cRow = blockIdx.y * BLOCKSIZE;
+    const int cCol = blockIdx.x * BLOCKSIZE;
+
+    A += cRow * k;
+    B += cCol;
+    C += cRow * n + cCol;
+
+    __shared__ float As[BLOCKSIZE * BLOCKSIZE];
+    __shared__ float Bs[BLOCKSIZE * BLOCKSIZE];
+
+    const int threadX = threadIdx.x % BLOCKSIZE;
+    const int threadY = threadIdx.y % BLOCKSIZE;
+
+    float acc = 0.0;
+    for(int chunkIdx = 0; chunkIdx < n; chunkIdx += BLOCKSIZE){
+        // prepare As and Bs
+        // Every thread is responsible for pulling one element
+        // But only do that if we're in bounds! Use 0 if not
+        // make sure everyone is done reading from As and Bs before we get back to it
+        __syncthreads();
+        if((chunkIdx+threadY < n) && (chunkIdx+threadX) < n) {
+            As[threadY * BLOCKSIZE + threadX] = A[(threadY) * k + (chunkIdx + threadX)];
+            Bs[threadY * BLOCKSIZE + threadX] = B[(threadY + chunkIdx) * n + threadX];
+        }
+        else {
+            As[threadY * BLOCKSIZE + threadX] = 0.0;
+            Bs[threadY * BLOCKSIZE + threadX] = 0.0;
+        }
+        __syncthreads();
+
+
+        // dot the xth row of A with the yth col of B
+        for(int i = 0; i < BLOCKSIZE; i++){
+            int aIndex = (threadY * BLOCKSIZE) + i;
+            int bIndex =  i*BLOCKSIZE + threadX;
+            acc += As[aIndex] * Bs[bIndex];
+        }
+    }
+    // Now we need to get back to the original coordinates
+    if(threadX < m && threadY < n){
+        C[threadY*n + threadX] = acc;
+    }
+}
+""".replace("BLOCKSIZE", str(BLOCKSIZE))
+
+pointer_kernel = cp.RawKernel(pointer_matmul_code, "pointer_matmul")
 
 multiple_per_thread_matmul_code = r"""
 extern "C" __global__
@@ -181,10 +230,11 @@ void multi_per_thread_matmul(int m, int k, int n, const float* A, const float* B
         // but with an extra iteration - really we care about xth + stride and yth+stride
         for(int dotIndex = 0; dotIndex < chunkStride; dotIndex++){
             for(int threadOffsetY = 0; threadOffsetY < THREAD_WINDOW; threadOffsetY += 1){
+                int aIndex = (threadY*THREAD_WINDOW + threadOffsetY) * chunkStride + (dotIndex);
+                float aVal = As[aIndex];
                 for(int threadOffsetX = 0; threadOffsetX < THREAD_WINDOW; threadOffsetX += 1){
-                    int aIndex = (threadY*THREAD_WINDOW + threadOffsetY) * chunkStride + (dotIndex);
                     int bIndex = (dotIndex) * chunkStride + (threadX*THREAD_WINDOW + threadOffsetX);
-                    threadResults[threadOffsetY*THREAD_WINDOW + threadOffsetX] += As[aIndex] * Bs[bIndex];
+                    threadResults[threadOffsetY*THREAD_WINDOW + threadOffsetX] += aVal * Bs[bIndex];
                 }
             }
         }
@@ -209,11 +259,116 @@ multi_per_thread_kernel = cp.RawKernel(
     multiple_per_thread_matmul_code, "multi_per_thread_matmul"
 )
 
+matmul_code = r"""
+extern "C" __global__
+void matmul(int m, int k, int n, int batches, const float* A, const float* B, float* C) {
+    // Implements matrix multiplication with shared memory and multiple C indices handled by one thread.
+    // Works one chunk at a time, looking at a chunkSizexchunkSize block of A and B, and storing their
+    // various rowxcol dot products in threadResults. By sliding chunk along the cols of A and the rows
+    // of B, we accumulate the entire dot product for each row and col.
+    // One extra wrinkle: We need to handle batched multiplications. We assume the last two dimensions
+    // are the two actually being multiplied, everything else is batch dims. That's collapsed down into
+    // the batches argument. We're in row-major world here, so moving to a new batch means we need to advance
+    // our pointers by the product of the two final dims
+    const int batchStepA = m*k;
+    const int batchStepB = k*n;
+    const int batchStepC = m*n;
 
-size = 10000
-A = cp.random.random((size, size), dtype=cp.float32)
-B = cp.random.random((size, size), dtype=cp.float32)
-C = cp.empty((A.shape[0], B.shape[1]), dtype=cp.float32)
+    // Some template variables:
+    // BLOCKSIZE is the underlying block size - assuming our block is square
+    // THREAD_WINDOW is how many X or Y indices each thread is responsible for (we're making square windows)
+    // Therefore, each block is really responsible for (BLOCKSIZE*THREAD_WINDOW)^2 entries
+
+    // How big one chunk is
+    const int chunkSize = BLOCKSIZE * THREAD_WINDOW;
+
+    // Where this block's work starts, in global coords
+    const int blockRow = blockIdx.y * chunkSize;
+    const int blockCol = blockIdx.x * chunkSize;
+
+    // Go ahead and adjust A and B to account for where the blocks start
+    A += blockRow * k;
+    B += blockCol;
+
+    // Shared memory for this block - we'll load chunks of A and B into here so that we don't
+    // waste time repeatedly querying global memory
+    // This is chunkSize^2, but we can't define it as such since that's a variable
+    __shared__ float As[BLOCKSIZE * BLOCKSIZE * THREAD_WINDOW * THREAD_WINDOW];
+    __shared__ float Bs[BLOCKSIZE * BLOCKSIZE * THREAD_WINDOW * THREAD_WINDOW];
+
+    // Where this thread starts in chunk coordinates. Each thread will handle a THREAD_WINDOWxTHREAD_WINDOW
+    // square of dot products - this lets each thread handle more math, reducing memory pressure
+    const int threadX = (threadIdx.x % BLOCKSIZE) * THREAD_WINDOW;
+    const int threadY = (threadIdx.y % BLOCKSIZE) * THREAD_WINDOW;
+
+    for(int batch = 0; batch < batches; batch++){
+        // accumulate our partial dot products here
+        float threadResults[THREAD_WINDOW * THREAD_WINDOW] = {0.0};
+
+        for(int chunkIdx = 0; chunkIdx < n; chunkIdx += chunkSize){
+            // Assume that our pointers for A and B are always pointing to the first entry of the chunk we care about. 
+
+            // Load the next chunk of A and B into shared memory. If we've run off the edge of the matrix load 0s instead
+            // Each thread is responsible for loading THREAD_WINDOW^2 values
+            // And sync immediately before and after writing to shared mem, to make sure all threads are viewing 
+            // fresh data
+            __syncthreads();
+            for(int colOffset = 0; colOffset < THREAD_WINDOW; colOffset += 1){
+                for(int rowOffset = 0; rowOffset < THREAD_WINDOW; rowOffset += 1){
+                    int localIndex = (threadY + rowOffset) * chunkSize + (threadX + colOffset);
+                    bool inBoundsK = chunkIdx+threadY < k && (chunkIdx+threadX) < k;
+                    bool inBoundsM = (blockRow+threadY + rowOffset) < m;
+                    bool inBoundsN = (blockCol+threadX + rowOffset) < n;
+                    if(inBoundsM && inBoundsK){
+                        As[localIndex] = A[(threadY + rowOffset) * k + (threadX + colOffset + chunkIdx)];
+                    } else{
+                        As[localIndex] = 0.0;
+                    }
+                    if(inBoundsN && inBoundsK){
+                        Bs[localIndex] = B[(threadY + rowOffset + chunkIdx) * n + (threadX + colOffset)];
+                    } else {
+                        Bs[localIndex] = 0.0;
+                    }
+                }
+            }
+            __syncthreads();
+
+            //for each thread, we need to dot the xth col of b with the yth row of a
+            // but with two extra iterations - really we care about xth + stride and yth+stride
+            for(int dotIndex = 0; dotIndex < chunkSize; dotIndex++){
+                for(int threadOffsetY = 0; threadOffsetY < THREAD_WINDOW; threadOffsetY += 1){
+                    int aIndex = (threadY + threadOffsetY) * chunkSize + (dotIndex);
+                    float aVal = As[aIndex];
+                    for(int threadOffsetX = 0; threadOffsetX < THREAD_WINDOW; threadOffsetX += 1){
+                        int bIndex = (dotIndex) * chunkSize + (threadX + threadOffsetX);
+                        threadResults[threadOffsetY*THREAD_WINDOW + threadOffsetX] += aVal * Bs[bIndex];
+                    }
+                }
+            }
+        }
+        // Now we have to pull threadResults into C
+        for(int threadOffsetY = 0; threadOffsetY < THREAD_WINDOW; threadOffsetY += 1){
+            for(int threadOffsetX = 0; threadOffsetX < THREAD_WINDOW; threadOffsetX += 1){
+                int cX = (blockCol + threadX + threadOffsetX);
+                int cY = (blockRow + threadY + threadOffsetY);
+                if(cX < n && cY < m){
+                    C[cY * n + cX] = threadResults[threadOffsetY * THREAD_WINDOW + threadOffsetX];
+                }
+            }
+        }
+        A += batchStepA;
+        B += batchStepB;
+        C += batchStepC;
+    }
+}
+""".replace("BLOCKSIZE", str(BLOCKSIZE)).replace("THREAD_WINDOW", str(THREAD_WINDOW))
+
+matmul_kernel = cp.RawKernel(matmul_code, "matmul")
+
+size = 2000
+A = cp.random.random((2, size, size), dtype=cp.float32)
+B = cp.random.random((2, size, size), dtype=cp.float32)
+C = cp.empty((2, A.shape[-2], B.shape[-1]), dtype=cp.float32)
 
 # A = cp.array([[1.0, 2.0], [3.0, 4.0]], dtype=cp.float32)
 # B = cp.array([[1.0, 2.0], [3.0, 4.0]], dtype=cp.float32)
@@ -225,31 +380,25 @@ start = time.time()
 result = nA @ nB
 print(result)
 duration = time.time() - start
-print("CPU took", duration)
 multigrid = (
     math.ceil(B.shape[-1] / (BLOCKSIZE * THREAD_WINDOW)),
     math.ceil(A.shape[-1] / (BLOCKSIZE * THREAD_WINDOW)),
 )
 grid = (
-    math.ceil(B.shape[-1] / (BLOCKSIZE * THREAD_WINDOW)),
-    math.ceil(A.shape[-1] / (BLOCKSIZE * THREAD_WINDOW)),
+    math.ceil(B.shape[-1] / (BLOCKSIZE)),
+    math.ceil(A.shape[-1] / (BLOCKSIZE)),
 )
 
-print(
-    benchmark(
-        shared_kernel,
-        (
-            grid,
-            (BLOCKSIZE, BLOCKSIZE),
-            (A.shape[0], A.shape[1], B.shape[1], A, B, C),
-        ),
-        n_repeat=10,
-        n_warmup=3,
-    )
-)
 
-print(
-    benchmark(
+matmul_kernel(
+    multigrid,
+    (BLOCKSIZE, BLOCKSIZE),
+    (A.shape[-2], A.shape[-1], B.shape[-1], 2, A, B, C),
+)
+print(C)
+
+if False:
+    multi_result = benchmark(
         multi_per_thread_kernel,
         (
             multigrid,
@@ -259,4 +408,9 @@ print(
         n_repeat=10,
         n_warmup=3,
     )
-)
+
+    v3_time = cp.mean(shared_result.gpu_times)
+    v4_time = cp.mean(multi_result.gpu_times)
+    print(f"CPU   took {duration:.4f} seconds")
+    print(f"GPUv3 took {v3_time:.4f} seconds ({(duration / v3_time):.1f}x faster)")
+    print(f"GPUv4 took {v4_time:.4f} seconds ({(duration / v4_time):.1f}x faster)")
