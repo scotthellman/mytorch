4e0eaedf8564a25b8a5fd5c09fd1dc6c3f5e059b
scott <scotthellman@fastmail.com>
2026-02-17 11:26:39 -0600

    updated tests

--- tests/test_gpu_layers.py
+++ tests/test_gpu_layers.py
@@ -6,7 +6,7 @@ from mytorch.gpu_tensor import GpuTensor
 
 def test_linear():
     layer = gpu_layers.Linear(3, 2, False)
-    vec = GpuTensor(cp.array([[1, 2, 3]], dtype=cp.float32).reshape(1, 1, 3))
+    vec = GpuTensor(cp.array([[1, 2, 3, 4, 5, 6]], dtype=cp.float32).reshape(2, 1, 3))
 
     expected = vec.value @ layer.weights.value
     actual = layer.forward(vec)
@@ -24,9 +24,15 @@ def test_linear():
 
 def test_sigmoid():
     layer = gpu_layers.Sigmoid()
-    vec = GpuTensor(cp.array([[0, 1, 2]], dtype=cp.float32).reshape(1, 1, 3))
+    vec = GpuTensor(cp.array([[0, 1, 2, 3, 4, 5]], dtype=cp.float32).reshape(2, 1, 3))
 
     expected = 1 / (1 + cp.exp(-vec.value))
     actual = layer.forward(vec)
 
     assert cp.allclose(expected, actual.value)
+
+    ONE = cp.array([1.0], dtype=cp.float32)
+    assert len(actual.operations) == 1
+    backpropped = actual.operations[0][2](ONE)
+    expected = expected * (1 - expected)
+    assert cp.allclose(expected, backpropped)
--- tests/test_gpu_tensor.py
+++ tests/test_gpu_tensor.py
@@ -13,22 +13,26 @@ def build_gradient_lookup(ops):
 
 
 def test_add():
-    a = GpuTensor(cp.array([1.0, 2.0, 3.0]))
-    b = GpuTensor(cp.array([3.0]))
+    a = GpuTensor(cp.arange(8, dtype=cp.float32).reshape(2, 2, 2))
+    b = GpuTensor(cp.array([3, 3], dtype=cp.float32).reshape(2, 1, 1))
     expected = a.value + b.value
     result = a + b
     assert cp.all(expected == result.value)
 
     gradient_lookup = build_gradient_lookup(result.operations)
 
-    expected_a_grad = cp.array([1])
-    actual_a_grad = gradient_lookup[a](ONE)
+    expected_a_grad = cp.ones_like(a.value)
+    batch_one = cp.ones_like(a.value)
+    actual_a_grad = gradient_lookup[a](batch_one)
 
+    assert expected_a_grad.shape == actual_a_grad.shape
     assert cp.all(expected_a_grad == actual_a_grad)
 
-    expected_b_grad = cp.array([1])
-    actual_b_grad = gradient_lookup[b](ONE)
+    batch_one = cp.ones_like(b.value)
+    expected_b_grad = cp.ones_like(b.value)
+    actual_b_grad = gradient_lookup[b](batch_one)
 
+    assert expected_b_grad.shape == actual_b_grad.shape
     assert cp.all(expected_b_grad == actual_b_grad)
 
 
@@ -123,6 +127,28 @@ def test_matmul():
     assert cp.all(expected_a_grad == actual_a_grad)
 
     expected_b_grad = cp.array([[1.0], [2.0], [3.0]])
+    actual_b_grad = gradient_lookup[b](grad_input)
+
+    assert cp.all(expected_b_grad == actual_b_grad)
+
+
+def test_batch_matmul():
+    a = GpuTensor(cp.arange(12, dtype=cp.float32).reshape(2, 2, 3))
+    b = GpuTensor(cp.arange(12, dtype=cp.float32).reshape(2, 3, 2))
+    expected = a.value @ b.value
+    result = a @ b
+    assert cp.all(expected == result.value)
+
+    gradient_lookup = build_gradient_lookup(result.operations)
+
+    grad_input = cp.ones((2, 2, 2), dtype=cp.float32)
+
+    expected_a_grad = grad_input @ cp.swapaxes(b.value, -1, -2)
+    actual_a_grad = gradient_lookup[a](grad_input)
+
+    assert cp.all(expected_a_grad == actual_a_grad)
+
+    expected_b_grad = cp.swapaxes(a.value, -1, -2) @ grad_input
     actual_b_grad = gradient_lookup[b](grad_input)
 
     assert cp.all(expected_b_grad == actual_b_grad)
