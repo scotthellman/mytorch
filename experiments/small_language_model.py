5ebeb4341ec906a70cef10adfdaae2b560a6da3a
scott <scotthellman@fastmail.com>
2026-03-03 19:42:27 -0600

    kind of a real (small) llm

--- experiments/small_language_model.py
+++ experiments/small_language_model.py
@@ -1,4 +1,6 @@
 import pickle
+import random
+import time
 
 import cupy as cp
 import numpy as np
@@ -6,19 +8,19 @@ from tqdm import tqdm
 
 from mytorch import layers, optimizers
 from mytorch.tensor import Tensor
-from mytorch.tokenizers import NaiveBPE
+from mytorch.tokenizers import BPE
 
-with open("/home/scott/Downloads/alice_in_wonderland.txt", "r") as f:
+with open("processed_text.txt", "r") as f:
     text = f.read()
 
 text = text.encode("utf-8")
 
-vocab_size = 4096
+vocab_size = 32768 // 2
 embedding_size = 256
 lr = 2e-4
-seq_length = 64
-batch_size = 32
-epochs = 1000
+seq_length = 128
+batch_size = 16
+epochs = 100
 
 loss_func = layers.CrossEntropyLoss()
 network = [
@@ -32,32 +34,51 @@ network = [
 
 network[-1].weights = network[0].weights.transpose_last()
 params = [p for layer in network for p in layer.params]
-optimizer = optimizers.Adam([(params, lr, 0.01)], warmup_steps=500)
+optimizer = optimizers.Adam([(params, lr, 0.01)], warmup_steps=1000)
 
-if False:
-    tokenizer = NaiveBPE(vocab_size)
-    tokenizer.fit([text])
+redo_vocab = False
+
+if redo_vocab:
+    tokenizer = BPE(vocab_size)
+    start = time.time()
+    tokenizer.fit(text)
+    print("took", time.time() - start)
     with open("tokenizer.pkl", "wb") as f:
         pickle.dump(tokenizer, f)
+    tokenized_text = []
+    chunk_size = 1000000
+    for i in range(0, len(text), chunk_size):
+        tokenized_subtext = tokenizer.tokenize(text[i : i + chunk_size])
+        tokenized_text.extend(tokenized_subtext)
+    with open("tokenized.pkl", "wb") as f:
+        pickle.dump(tokenized_text, f)
 else:
     with open("tokenizer.pkl", "rb") as f:
         tokenizer = pickle.load(f)
-
-tokenized_text = tokenizer.tokenize(text)
+    with open("tokenized.pkl", "rb") as f:
+        tokenized_text = pickle.load(f)
 
 X = []
 for i in range(0, len(tokenized_text), seq_length):
     X.append(tokenized_text[i : i + seq_length])
+# last row is unlikely to have the right length
+X = X[:-1]
+# TODO: I really should seed all the randomness
 
-# chuck the last row, it won't have the right shape
-X = cp.array(X[:-1], dtype=cp.int32)
+monitor_toks = tokenizer.tokenize(
+    b"This is a simple sentence, nothing too weird going on here."
+)
+monitor_tensor = Tensor(cp.array(monitor_toks)[None, :])
+
+random.shuffle(X)
 best_loss = float("inf")
-
+iterations = 0
 for e in range(epochs):
     losses = []
-    for i in tqdm(range(0, X.shape[0], batch_size), total=X.shape[0] // batch_size):
-        X_batch = Tensor(X[i : i + batch_size, :-1])
-        y_batch = Tensor(X[i : i + batch_size, 1:])
+    for i in tqdm(range(0, len(X), batch_size), total=len(X) // batch_size):
+        full_batch = cp.array(X[i : i + batch_size], dtype=cp.int32)
+        X_batch = Tensor(full_batch[:, :-1])
+        y_batch = Tensor(full_batch[:, 1:])
 
         processed = X_batch
         for j, layer in enumerate(network):
@@ -67,19 +88,20 @@ for e in range(epochs):
         losses.append(float(loss.value))
         loss.compute_gradient()
         optimizer.step()
-    mean_loss = np.mean(losses)
-    print(f"mean train loss for epoch {e} was {mean_loss}")
-    if mean_loss < best_loss:
-        old_ops = network[-1].weights.operations
-        network[-1].weights.operations = []
-        with open("network.pkl", "wb") as f:
-            pickle.dump(network, f)
-        network[-1].weights.operations = old_ops
-    print(tokenizer.untokenize(X[20].tolist()))
+        if iterations % 100 == 0:
+            mean_loss = np.mean(losses)
+            print("mean loss", mean_loss)
+            losses = []
+            if mean_loss < best_loss:
+                old_ops = network[-1].weights.operations
+                network[-1].weights.operations = []
+                with open("network.pkl", "wb") as f:
+                    pickle.dump(network, f)
+                network[-1].weights.operations = old_ops
 
-    processed = Tensor(X[20][None, :])
-    for j, layer in enumerate(network):
-        processed = layer.forward(processed)
-    pred = cp.argmax(processed.value, axis=-1)[0]
-    # print(processed.value.max())
-    print(tokenizer.untokenize(pred.tolist()))
+            processed = monitor_tensor
+            for j, layer in enumerate(network):
+                processed = layer.forward(processed)
+            pred = cp.argmax(processed.value, axis=-1)[0]
+            print(tokenizer.untokenize(pred.tolist()))
+        iterations += 1
