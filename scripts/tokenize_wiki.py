10387353cdfbce7144264ad7d51e17f4ce37ce0a
scott <scotthellman@fastmail.com>
2026-03-10 13:28:55 -0500

    wiki tokenization script

---
+++ scripts/tokenize_wiki.py
@@ -0,0 +1,65 @@
+import bz2
+import pickle
+import re
+import time
+from pathlib import Path
+
+import mwxml
+from tqdm import tqdm, trange
+
+from mytorch.tokenizers import BPE
+
+tqdm.set_lock(tqdm.get_lock())
+
+vocab_size = 32768 // 2
+target_dir = Path("/home/scott/Downloads/wiki")
+
+ref_pattern = re.compile(r"\{\{(?:[^{}]|\{\{[^{}]*\}\})*\}\}", flags=re.DOTALL)
+bad_start_pattern = re.compile(r"^[=|*!{<].*", re.MULTILINE)
+empty_pattern = re.compile(r"^\s*$", re.MULTILINE)
+
+
+# TODO: if i changed my tokenier to take an iterable of texts,
+# we could work with the full dump. But for now, just grab the first
+# few thousand articles
+# 100 articles is roughly 2 megs
+
+print("Pulling data from bzip")
+texts = []
+for fn in target_dir.glob("*.bz2"):
+    with bz2.open(fn) as f:
+        dump = mwxml.Dump.from_file(f)
+        for page in dump:
+            if page.redirect is not None:
+                continue
+            for revision in page:
+                text = revision.text
+                text = re.sub(ref_pattern, "", text)
+                text = re.sub(bad_start_pattern, "", text)
+                text = re.sub(empty_pattern, "", text)
+                text = text.replace("[[", "")
+                text = text.replace("]]", "")
+
+                texts.append(text)
+            if len(texts) > 10000:
+                break
+
+final_text = "\n".join(texts)
+final_text = final_text.encode("utf-8")
+print("final text is of length", len(final_text))
+del texts
+
+tokenizer = BPE(vocab_size)
+start = time.time()
+tokenizer.fit(final_text)
+print("vocab learning took", time.time() - start)
+with open("tokenizer.pkl", "wb") as f:
+    pickle.dump(tokenizer, f)
+
+chunk_size = 500000
+tokenized_text = []
+for i in trange(0, len(final_text), chunk_size, position=0):
+    tokenized_subtext = tokenizer.tokenize(final_text[i : i + chunk_size], pbar_pos=1)
+    tokenized_text.extend(tokenized_subtext)
+with open("tokenized.pkl", "wb") as f:
+    pickle.dump(tokenized_text, f)
