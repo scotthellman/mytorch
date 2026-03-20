28161a12d1d58a1a333ab206202f8eefd47ab84e
scott <scotthellman@fastmail.com>
2026-03-20 13:16:09 -0500

    making the inference script actually print its result

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,14 +1,16 @@
 import pickle
-import time
+from pathlib import Path
 
 import click
-from mytorch import layers
+
 from mytorch.inference import Decoder
 from mytorch.tensor import DisableGradManager
 from mytorch.tokenizers.tokenizers import BPE
-from pathlib import Path
 
-def generate_text(prefix: bytes, tokenizer_fn: Path, network_fn: Path, temperature: float = 0.1):
+
+def generate_text(
+    prefix: bytes, tokenizer_fn: Path, network_fn: Path, temperature: float = 0.1
+) -> bytes:
     with open(tokenizer_fn, "rb") as f:
         tokenizer: BPE = pickle.load(f)
     with open(network_fn, "rb") as f:
@@ -16,16 +18,27 @@ def generate_text(prefix: bytes, tokenizer_fn: Path, network_fn: Path, temperatu
     decoder = Decoder(network, tokenizer)
 
     with DisableGradManager():
-        decoder.sample_text(prefix, 200, True, temperature=temperature)
+        result = decoder.sample_text(prefix, 200, True, temperature=temperature)
+    return result
 
 
 @click.command()
 @click.argument("prefix")
-@click.option("--tokenizer", "tokenizer_fn", required=True, type=click.Path(exists=True, path_type=Path))
-@click.option("--network", "network_fn", required=True, type=click.Path(exists=True, path_type=Path))
+@click.option(
+    "--tokenizer",
+    "tokenizer_fn",
+    required=True,
+    type=click.Path(exists=True, path_type=Path),
+)
+@click.option(
+    "--network",
+    "network_fn",
+    required=True,
+    type=click.Path(exists=True, path_type=Path),
+)
 @click.option("--temperature", default=0.1, show_default=True)
 def main(prefix: str, tokenizer_fn: Path, network_fn: Path, temperature: float):
-    generate_text(prefix.encode(), tokenizer_fn, network_fn, temperature)
+    print(generate_text(prefix.encode(), tokenizer_fn, network_fn, temperature))
 
 
 if __name__ == "__main__":
